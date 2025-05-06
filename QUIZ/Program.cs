f11a92bb5496e562448d32df2bc0df8f305141c0
Wojciech Baron <wojciech.baron@terranova.edu.pl>
2025-05-06 12:25:17 +0200

    zadanie do wykonania

--- QUIZ/Program.cs
+++ QUIZ/Program.cs
@@ -3,14 +3,7 @@ using System.Text.Json.Serialization;
 
 class Quiz
 {
-    [JsonPropertyName("pytanie")]
-    public string Pytanie { get; set; }
-
-    [JsonPropertyName("odpowiedzi")]
-    public Dictionary<string, string> Odpowiedzi { get; set; }
-
-    [JsonPropertyName("poprawna")]
-    public string Poprawna { get; set; }
+ 
 }
 
 class Program
@@ -29,24 +22,7 @@ class Program
 
             foreach (var pyt in quiz)
             {
-                Console.WriteLine("\n" + pyt.Pytanie);
-                foreach (var odp in pyt.Odpowiedzi)
-                {
-                    Console.WriteLine($"{odp.Key}. {odp.Value}");
-                }
-
-                Console.Write("Twoja odpowiedź (A/B/C/D): ");
-                string odpUzytkownika = Console.ReadLine().ToUpper();
-
-                if (odpUzytkownika == pyt.Poprawna.ToUpper())
-                {
-                    Console.WriteLine("Poprawna!");
-                    punkty++;
-                }
-                else
-                {
-                    Console.WriteLine($"Błędna. Poprawna odpowiedź: {pyt.Poprawna}");
-                }
+               
             }
 
             Console.WriteLine($"\nTwój wynik: {punkty} / {quiz.Count}");
