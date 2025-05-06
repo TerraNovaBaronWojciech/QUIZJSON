0ea41ecdd8ce45e544b312f4269506863e8fc92c
Wojciech Baron <wojciech.baron@terranova.edu.pl>
2025-05-06 12:44:46 +0200

    add quiz

--- QUIZ/Program.cs
+++ QUIZ/Program.cs
@@ -1,31 +1,29 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
 
 class Quiz
 {
- 
+
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        string filePath = "\\quiz.json";
+        string filePath = ".\\Quiz.json";
 
-        if (File.Exists(filePath))
+       if (File.Exists(filePath))
         {
 
-            string json = File.ReadAllText(filePath);
-            var quiz = JsonSerializer.Deserialize<List<Quiz>>(json);
-
+            
             int punkty = 0;
 
-            foreach (var pyt in quiz)
+            //foreach (var pyt in quiz)
             {
                
             }
 
-            Console.WriteLine($"\nTwój wynik: {punkty} / {quiz.Count}");
+            
         }
     }
 }
